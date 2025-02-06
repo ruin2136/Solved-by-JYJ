d31a05d50c10805ce118c7a8faada8717e788e1e
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-06 14:19:08 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+    cin.tie(NULL);  // cin과 cout의 묶음을 해제
+    
+    int a, b, c;
+
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        cin >> b >> c;
+        cout <<"Case #"<<i<<": " << b + c << "\n";
+    }
+}
