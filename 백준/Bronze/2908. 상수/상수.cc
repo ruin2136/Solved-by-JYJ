a2f53ed8593c31eb51bfad48670c06545505ffb2
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-07 11:10:06 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,29 @@
+#include <iostream>			//기본 입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <cctype>			//tolower()
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	string str1,str2;
+	string a, b;
+	vector<string> set;
+
+	cin >> str1>>str2;
+
+	for (int i=2;i>=0;i--)
+	{
+		a += str1[i];
+		b += str2[i];
+	}
+	if (stoi(a) > stoi(b))
+		cout << a;
+	else
+		cout << b;
+}
