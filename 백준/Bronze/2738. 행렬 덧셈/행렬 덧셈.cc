a3c53acbea3a4969906a2cb7bea403278d4d7625
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-10 19:50:05 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,49 @@
+#include <iostream>			//기본입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <unordered_map>	//unordered_set
+#include <cctype>			//tolower()
+#include <algorithm>		//transform()
+#include <map>				//map
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	int n, m;
+	vector<vector<int>> ary1;
+	vector<vector<int>> ary2;
+
+	cin >> n >> m;
+
+	ary1.resize(n, vector<int>(m));
+	ary2.resize(n, vector<int>(m));
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> ary1[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> ary2[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout<<ary1[i][j]+ary2[i][j]<<" ";
+		}
+		cout << "\n";
+	}
+}
