a5d6771ff7429a4ffa87c3fd4395e79f8fe0e581
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-15 21:43:31 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,33 @@
+#include <iostream>			//기본입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <unordered_map>	//unordered_set
+#include <cctype>			//tolower()
+#include <algorithm>		//transform()
+#include <map>				//map
+#include <cmath>			//pow
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	int num, result=0, max=1;
+
+	cin >> num;
+
+	while(max<num)
+	{
+		max += 6*result;
+
+		result++;
+	}
+
+	if (num == 1)
+		result = 1;
+
+	cout << result;
+}
