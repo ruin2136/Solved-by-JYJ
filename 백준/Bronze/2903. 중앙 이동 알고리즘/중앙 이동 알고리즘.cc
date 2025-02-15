84095be275b0efa253b58001299fdaa9799dc831
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-15 21:14:19 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,30 @@
+#include <iostream>			//기본입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <unordered_map>	//unordered_set
+#include <cctype>			//tolower()
+#include <algorithm>		//transform()
+#include <map>				//map
+#include <cmath>			//pow
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	int num, total;
+
+	cin >> num;
+
+	total = 2;
+
+	for (int i = 0; i < num; i++)
+	{
+		total += pow(2, i);
+	}
+
+	cout << total*total;
+}
