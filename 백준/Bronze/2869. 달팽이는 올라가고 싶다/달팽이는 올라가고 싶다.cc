f24c3b5c0b171267bebfea459c49a62dd3e01f2c
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-18 03:54:51 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,25 @@
+#include <iostream>			//기본입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <unordered_map>	//unordered_set
+#include <cctype>			//tolower()
+#include <algorithm>		//transform()
+#include <map>				//map
+#include <cmath>			//pow
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	int a, b, v, result=0;
+
+	cin >> a>>b>>v;
+
+	result = ceil(double(v-a) / (a - b))+1;
+
+	cout << result;
+}
