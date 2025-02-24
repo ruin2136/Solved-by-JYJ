bbbeb4b08e3509063edf39151d268f23de427d74
ruin2136 <108153009+ruin2136@users.noreply.github.com>
2025-02-24 22:11:33 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,23 @@
+#include <iostream>			//기본입출력
+#include <vector>			//vector
+#include <sstream>			//istringstream
+#include <unordered_set>	//unordered_set
+#include <unordered_map>	//unordered_set
+#include <cctype>			//tolower()
+#include <algorithm>		//transform(), sort()
+#include <map>				//map
+#include <cmath>			//pow
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);   //C와 C++ 묶음을 해제
+	cin.tie(NULL);  // cin과 cout의 묶음을 해제
+
+	int a, b;
+
+	cin >> a>>b;
+
+	cout << a * b;
+}
